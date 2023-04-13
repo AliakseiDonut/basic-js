cae08056df57c748c2949efd293b7b77c024ad72
aliakseidonut <alexeydomut@gmail.com>
2023-04-13 18:00:56 +0300

    carbon-datings, count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const res = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  if(+sampleActivity > HALF_LIFE_PERIOD || isNaN(+sampleActivity) || res == Infinity || +sampleActivity <= 0 || typeof sampleActivity != 'string' || res < 0){
+    return false;
+  }
+  return res;
 }
 
 module.exports = {
