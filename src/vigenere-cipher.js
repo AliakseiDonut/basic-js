186efdc82735d356d8e8cc5cab7e0ef500fe961d
aliakseidonut <alexeydomut@gmail.com>
2023-04-13 22:56:58 +0300

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,17 +19,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+const ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const repeatKey = (key, length) => {
+  while(key.length < length){
+    key += key;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return key.slice(0, length);
+}
+
+class VigenereCipheringMachine {
+  constructor(boolean = true) {
+    this.reverse = boolean;
+  }
+  encrypt(word , key) {
+    if(!word || !key){
+      throw new Error('Incorrect arguments!');
+    }
+    word = word.toUpperCase();
+    let arr = word.split('').filter(el => ALPHABET.includes(el));
+    key = key.toUpperCase();
+    key = repeatKey(key, arr.length)
+    let i = 0;
+    let res = word.split('').map(el => {
+      if(ALPHABET.includes(el)){
+        return ALPHABET[(ALPHABET.indexOf(el) + ALPHABET.indexOf(key[i++])) % 26];
+      }else{
+        return el;
+      }
+    });
+    if(this.reverse){
+      return res.join('');
+    }else{
+      return res.reverse().join('');
+    }
+  }
+  decrypt(word , key) {
+    if(!word || !key){ 
+      throw new Error('Incorrect arguments!'); 
+    } 
+    word = word.toUpperCase(); 
+    let arr = word.split('').filter(el => ALPHABET.includes(el)); 
+    key = key.toUpperCase(); 
+    key = repeatKey(key, arr.length) 
+    let i = 0; 
+    let res = word.split('').map(el => { 
+      if(ALPHABET.includes(el)){ 
+        return ALPHABET[(ALPHABET.indexOf(el) - ALPHABET.indexOf(key[i++]) + 26) % 26]; 
+      }else{ 
+        return el; 
+      } 
+    }) 
+    if(this.reverse){ 
+      return res.join(''); 
+    }else{ 
+      return res.reverse().join(''); 
+    } 
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
