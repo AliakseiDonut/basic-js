e884be58ce40085c66c21ce24d7114234d59d297
aliakseidonut <alexeydomut@gmail.com>
2023-04-15 14:29:15 +0300

    extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  
+  let res = '';
+  
+  if(options.addition && !options.additionRepeatTimes){
+    options.additionRepeatTimes = 1;
+  }
+
+  if(!options.repeatTimes){
+    options.repeatTimes = 1;
+  }
+
+  if(!options.additionSeparator){
+    options.additionSeparator = '|';
+  }
+
+  if(!options.separator){
+    options.separator = '+';
+  }
+
+  for(let i = 0; i < options.repeatTimes; i++){
+    res += str;
+    
+    if(options.additionRepeatTimes){
+      for(let j = 0; j < options.additionRepeatTimes; j++){
+
+        res += options.addition;
+
+        if(j != options.additionRepeatTimes - 1){
+          res += options.additionSeparator;
+        }
+      
+      }
+    } 
+    
+    if(options.separator && i != options.repeatTimes - 1){
+      res += options.separator;
+    }
+  }
+  
+  return res;
 }
 
 module.exports = {
